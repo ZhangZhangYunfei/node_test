dd750a5585b1f96404ca250c9baefdfe0a542343
yunfei <yunfei.zhang@dianrong.com>
2018-01-29 19:03:07 +0800

    revise api

--- routes/subject.js
+++ routes/subject.js
@@ -3,8 +3,21 @@ var router = express.Router();
 var DB = require('../utils/db');
 var Json = require('../utils/json');
 
-// 查询老师下某个学生所有课程
 router.get('/', function (req, res, next) {
+  var sql = 'SELECT s2.name as student_name, s.* '
+            + 'FROM subject s left join student s2 on s.student_id = s2.id '
+            + 'WHERE teacher_id = ?';
+  DB.query(sql, [req.session.userId], function (err, results, fields) {
+    if (err) {
+      res.end(Json.toString({status: 'FAILED', message: err.message}))
+    } else {
+      res.end(Json.toString(results));
+    }
+  });
+});
+
+// 查询老师下某个学生所有课程
+router.get('/student', function (req, res, next) {
   var sql = 'SELECT s2.name as student_name, s.* '
             + 'FROM subject s left join student s2 on s.student_id = s2.id '
             + 'WHERE teacher_id = ? AND student_id = ?';
